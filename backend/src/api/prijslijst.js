01ad0eda19c2579a4ab9d5e6fbcdafad67550ac8
gemistrynl-coder <gemistrynl@gmail.com>
2025-09-30 00:44:12 +0200

    beschrijving van je wijziging

--- backend/src/api/prijslijst.js
+++ backend/src/api/prijslijst.js
@@ -17,7 +17,7 @@ const pool = mysql.createPool({
     port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database: process.env.MYSQL_DATABASE, // 👈 zo heet die bij jou
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
