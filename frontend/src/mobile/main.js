f7cc551b4d34a9c1993b3d266b6e72dbf4ff5799
gemistrynl-coder <gemistrynl@gmail.com>
2025-09-30 23:49:16 +0200

    $1

--- frontend/src/mobile/main.js
+++ frontend/src/mobile/main.js
@@ -1,3 +1,4 @@
+// src/desktop/main.js
 import { createApp } from 'vue'
 import App from './App.vue'
 import VCalendar from 'v-calendar'
@@ -5,14 +6,7 @@ import 'v-calendar/style.css'
 
 import router from './router'
 
-
-
-if (/Mobi|Android|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent)) {
-    // 🚨 Redirect naar mobile.html als het een mobiel apparaat is
-    window.location.replace('/mobile.html')
-} else {
-    const app = createApp(App)
-    app.use(VCalendar, {})
-    app.use(router)
-    app.mount('#app')
-}
+const app = createApp(App)
+app.use(VCalendar, {})
+app.use(router)
+app.mount('#app')
