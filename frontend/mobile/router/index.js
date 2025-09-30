555a0bc1f3b8eb2eb65c9bfc4e41a2defd695df4
gemistrynl-coder <gemistrynl@gmail.com>
2025-09-30 20:59:28 +0200

    mobile desktop

--- frontend/mobile/router/index.js
+++ frontend/mobile/router/index.js
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// 👇 relative paths naar mobile/src/pages
-import Home from '../src/pages/home.vue'
-import ContactUs from '../src/pages/contact_us.vue'
-import OurServices from '../src/pages/our_services.vue'
-import OverOns from '../src/pages/over_ons.vue'
+import Home from '@/pages/home.vue'
+import ContactUs from '@/pages/contact_us.vue'
+import OurServices from '@/pages/our_services.vue'
+import OverOns from '@/pages/over_ons.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
@@ -19,3 +18,4 @@ const router = createRouter({
 })
 
 export default router
+lt router
